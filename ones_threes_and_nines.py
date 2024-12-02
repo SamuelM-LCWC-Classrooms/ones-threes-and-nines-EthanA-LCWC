f78398af3fbbe1700baa951d719dd6f137c25245
EthanA-LCWC <144792227+EthanA-LCWC@users.noreply.github.com>
2024-12-02 13:46:42 +0000

    I had to import a function to do this

--- ones_threes_and_nines.py
+++ ones_threes_and_nines.py
@@ -1,1 +1,6 @@
-
+import math
+class Ones_threes_nines:
+    def __init__(self, number):
+        self.ones = number
+        self.threes = math.floor(number / 3)
+        self.nines = math.floor(number / 9)
